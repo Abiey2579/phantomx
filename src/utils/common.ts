cd49e60334b53dd9b33b43a8adbb9632cc839ead
Yahaya Muhammad Bello <mbyahya2579@gmail.com>
2025-04-25 15:38:15 +0100

    adds

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,4 @@
 // Function to remove <think> tags from reseponse
 export const removeThinkTags = (text: string) => {
-  return text.replace(/<think>.*?<\/think>/gs, "").trim();
+  return text.replace(/<think>.*?<\/think>/, "").trim();
 };
